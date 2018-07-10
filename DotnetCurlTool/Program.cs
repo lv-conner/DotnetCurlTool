e3e9e8d2c886188990690fc329b4d9d253851253
lv-conner <lvmh123456@gmail.com>
2018-07-10 11:28:08 +0800

    下载指定文件

--- DotnetCurlTool/Program.cs
+++ DotnetCurlTool/Program.cs
@@ -5,6 +5,8 @@ using System.Collections;
 using System.Threading;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.CommandLine;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace DotnetCurlTool
 {
@@ -20,6 +22,15 @@ namespace DotnetCurlTool
             IConfigurationBuilder configurationBuilder = new ConfigurationBuilder().AddCommandLine(args, mapping);
             IConfiguration configuration = configurationBuilder.Build();
             Console.WriteLine(configuration.GetValue<string>("header"));
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.chiphell.com/");
+            var bytes = client.GetByteArrayAsync("data/attachment/portal/201710/05/172741mion2l2idnerm7ye.png").GetAwaiter().GetResult();
+            var files =  File.Open("hello.png", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            files.Write(bytes, 0, bytes.Length);
+
+
 
             Console.WriteLine("Hello World!");
         }
